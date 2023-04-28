e50e1e59ebbf1f4d0e610c14f067d78653fdb0ae
tvhardik <hardik@techvoot.com>
2023-04-28 19:40:48 +0530

    style,lifecycle methods,hooks

--- demo/App.tsx
+++ demo/App.tsx
@@ -4,9 +4,20 @@ import {Text, TextInput, View, Button, SafeAreaView} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: 20,
+        margin: 40,
+      }}>
       <View>
-        <Text style={{fontSize: 25}}> login page</Text>
+        {/* <ImageBackground
+          source={{
+            uri: 'https://e0.pxfuel.com/wallpapers/465/213/desktop-wallpaper-simple-mobile-flat-android.jpg',
+          }}></ImageBackground> */}
+      </View>
+      <View>
+        {/* <Text style={{fontSize: 25}}> login page</Text> */}
         <Text style={{fontSize: 25, textAlign: 'center'}}>Email ID</Text>
         <TextInput placeholder="Enter Your id" />
         <View>
