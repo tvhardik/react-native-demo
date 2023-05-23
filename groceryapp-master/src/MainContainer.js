1b83e99c27f43d25b59f2674d8d7369a67a2aadd
tvhardik <hardik@techvoot.com>
2023-05-23 19:37:28 +0530

    Code Deleted

--- groceryapp-master/src/MainContainer.js
+++
@@ -1,9 +0,0 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import AppNavigator from './AppNavigator';
-
-const MainContainer = () => {
-  return <AppNavigator />;
-};
-
-export default MainContainer;
